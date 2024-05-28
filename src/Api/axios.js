24b5f54265a90d465ca46f67bfecdd551f99b1d7
Desalegn Wendimu <asewen18@gmail.com>
2024-05-28 21:23:53 +0300

    catch all the redirects

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   //baseURL: "http://127.0.0.1:5001/clone-1d9f7/us-central1/api",
+  //baseUrl: "http://localhost:5000",
   // deployed verion of amazon  serever on render.com
   baseURL: "https://amazon-api-deploy-1-4nq9.onrender.com/",
 });
